c76ebc8302567b3fb2932a04d96b0ed836d8ed47
Fahad Ali <fahadali45621@gmail.com>
2026-03-28 08:53:02 +0500

    Extract quote from website

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+
+html_file = requests.get("https://quotes.toscrape.com/").text
+soup = BeautifulSoup(html_file,"lxml")
+quote = soup.select_one("div.quote")
+
+print(quote)
