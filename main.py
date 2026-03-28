6ec7ea7c9fc9b19a110a354c86029de023b853aa
Fahad Ali <fahadali45621@gmail.com>
2026-03-28 20:18:18 +0500

    Last commit

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
+import textwrap
+
+with open("quotes.csv","w",newline="",encoding="utf-8") as file:
+    writer = csv.writer(file)
+    writer.writerow(["Quote","Author","Tags","Birth Date","Birth Location"])
+
+    html_file = requests.get("https://quotes.toscrape.com/").text
+    soup = BeautifulSoup(html_file,"lxml")
+    quotes = soup.select("div.quote")
+
+    for quote in quotes:
+        title = quote.select_one("span.text").text.strip()
+        wrapped = textwrap.fill(title, width=40)
+        author_name = quote.select_one("small.author").text.strip()
+        tags = [tag.text for tag in quote.select("a.tag")]
 
 
-html_file = requests.get("https://quotes.toscrape.com/").text
-soup = BeautifulSoup(html_file,"lxml")
-quote = soup.select_one("div.quote")
+        author_link = quote.select_one("a")["href"]
+        full_link = "https://quotes.toscrape.com/"+ author_link
 
-print(quote)
+        author_html = requests.get(full_link).text
+        author_soup = BeautifulSoup(author_html,"lxml")
+
+        author_dob = author_soup.select_one("span.author-born-date").get_text()
+        author_location = author_soup.select_one("span.author-born-location").get_text()
+        
+        
+        writer.writerow([title, author_name,  ", ".join(tags) , author_dob, author_location])
